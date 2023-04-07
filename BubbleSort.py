1402600851a7d4e00ac9055298baf1c2d0d6cc00
jrvelloso96 <jrvelloso96@gmail.com>
2023-04-07 16:07:55 -0300

    primeiro commit

---
+++ BubbleSort.py
@@ -0,0 +1,22 @@
+import random
+
+
+def bubble_sort(v):
+    fim = len(v)
+    while fim > 0:
+        i = 0
+        print(v, "\n")
+        while i < fim-1:
+            if v[i] < v[i+1]:
+                temp = v[i]
+                v[i] = v[i+1]
+                v[i+1] = temp
+            i += 1
+        fim -= 1
+
+
+vetor = list(range(0, 10))
+random.shuffle(vetor)
+#print("Não ordenado \n", vetor, "\n")
+bubble_sort(vetor)
+#print("Ordenado \n", vetor)
